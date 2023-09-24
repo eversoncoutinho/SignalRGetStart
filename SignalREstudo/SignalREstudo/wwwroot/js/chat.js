eee5c2add123f5168f0dd4edb4dc34805d4e3fed
Everson Coutinho Coutinho <eversoncoutinho@gmail.com>
2023-09-24 12:59:41 +0100

    GetStart do doc

---
+++ SignalREstudo/SignalREstudo/wwwroot/js/chat.js
@@ -0,0 +1,29 @@
+﻿"use strict";
+
+//Cria e inicia uma conexão.
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Disable the send button until connection is established.
+document.getElementById("sendButton").disabled = true;
+
+//Objeto de conexão um manipulador que recebe mensagens do hub e as adiciona à lista.
+connection.on("ReceiveMessage", function (user, message) {
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    li.textContent = `${user} says ${message}`;
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
